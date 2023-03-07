dcd36f80b6f521d96db43d2721f23243a826a64b
ElchaX <maciej.sorko@gmail.com>
2023-03-07 20:15:15 +0100

    Both tasks done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (galleryItem) => `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}"
+     alt="${galleryItem.description}" />
+    </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+let stern = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionDelay: 250,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    
+});
